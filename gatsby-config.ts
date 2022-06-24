615b1c3e19bb580337ab6fa15a16932bda69aa7e
Jack Henderson <92jackhenderson@gmail.com>
2022-06-24 11:30:32 +0100

    gatsby wip

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,9 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `FEM artfulgarfunk`,
+    description: 'getting to know Gatsby',
+    image: 'https://en.wikipedia.org/wiki/File:Eurasian_wolf_2.jpg',
     siteUrl: `https://www.yourdomain.tld`,
   },
   graphqlTypegen: true,
